626e52803022e2eab8de53d953b8c969d9ec5f09
Tobias Schmitz <t.schmitz@chargecloud.de>
2023-03-10 16:57:54 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const rootDir = require('./util/path');
 
 const app = express();
 
-app.use('/add-product', (req, res) => {
-  res.send('<h1>Add-Product Page</h1>');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res) => {
-  res.send('<h1>Welcome</h1>');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir, 'public')))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
